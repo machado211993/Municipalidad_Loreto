054f207f5833a02557ffc9eba9af9258490a7bee
jose manuel machado <manuel.machado.j.m@gmail.com>
2024-12-25 15:52:41 -0300

    actualizacion 251224

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NovedadesComponent } from "../novedades/novedades.component";
 import { VideoComponent } from "../../components/video/video.component";
+import { EventoProximoComponent } from "../evento-proximo/evento-proximo.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, VideoComponent],
+  imports: [RouterLink, EventoProximoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
