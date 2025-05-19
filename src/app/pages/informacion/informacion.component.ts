9a9b300236ba0bd3d4e27ef3cf235d80baf1ecd6
Manuel <manuel.machado.j.m@gmail.com>
2025-05-19 02:31:03 -0300

    actualizacion190525

--- src/app/pages/informacion/informacion.component.ts
+++ src/app/pages/informacion/informacion.component.ts
@@ -6,51 +6,43 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [NgFor],
   templateUrl: './informacion.component.html',
-  styleUrls: ['./informacion.component.css'] // Corregido el nombre del atributo
+  styleUrls: ['./informacion.component.css'], // Corregido el nombre del atributo
 })
 export class InformacionComponent {
   informaciones: { descripcion: string; imagen: string }[] = [
     {
-      descripcion: "",
-      imagen: "assets/images/info1.jpeg",
+      descripcion: '',
+      imagen: 'assets/images/info7.jpeg',
+    },
+    {
+      descripcion: '',
+      imagen: 'assets/images/info1.jpeg',
     },
 
     {
-      descripcion: "Hoy vivimos una jornada increíble en el playón del Polideportivo Municipal 💪🏽, donde distintas áreas de la Municipalidad de Loreto brindaron atención y servicios a nuestros vecinos 🤝👨🏽‍💼 Estuvo presente el Intendente Prof. Orlando Maidana junto al equipo del CIC, Salud, Acción Social, Producción (con entrega de semillas 🌱), Oficina Móvil de Gestión, Ropero Solidario 👕, Turismo 🧭 y la Feria de Emprendedores 🛍️🎉 También compartimos Bingo, Truco 🃏 y cerramos con un delicioso chocolate para todos 🍫❤️ 🙏 Agradecemos al equipo de la Dirección General de Epidemiología del Ministerio de Salud de la Provincia por su valioso aporte.💚 !Seguimos trabajando cerca de la gente!",
-      imagen: "assets/images/info2.jpeg",
-    },
-    {
-      descripcion: "",
-      imagen: "assets/images/info3.jpeg",
+      descripcion: '',
+      imagen: 'assets/images/info3.jpeg',
     },
 
     {
-      descripcion: "",
-      imagen: "assets/images/info4.jpeg",
-    },
-   
-
-    {
-      descripcion: "",
-      imagen: "assets/images/info6.jpeg",
-    },
-     {
-      descripcion: "",
-      imagen: "assets/images/info5.jpeg",
-    },
-    {
-      descripcion: "",
-      imagen: "assets/images/actu1.jpeg",
+      descripcion: '',
+      imagen: 'assets/images/info6.jpeg',
     },
 
     {
-      descripcion: "",
-      imagen: "assets/images/actu4.jpeg",
+      descripcion: '',
+      imagen: 'assets/images/actu1.jpeg',
     },
 
-    
+    {
+      descripcion: '',
+      imagen: 'assets/images/actu4.jpeg',
+    },
+    {
+      descripcion: '',
+      imagen: 'assets/images/info5.jpeg',
+    },
 
-    
     // Agrega más informacion según sea necesario
   ];
 
@@ -58,7 +50,7 @@ export class InformacionComponent {
     const modal = document.getElementById('zoomModal');
     const zoomImage = document.getElementById('zoomImage') as HTMLImageElement;
     if (modal) {
-      modal.style.display = "block";
+      modal.style.display = 'block';
       zoomImage.src = imagen;
     }
   }
@@ -66,7 +58,7 @@ export class InformacionComponent {
   closeZoom() {
     const modal = document.getElementById('zoomModal');
     if (modal) {
-      modal.style.display = "none";
+      modal.style.display = 'none';
     }
   }
 }
