0144ffad89cb33cff20b7f78bc34944dbb129c5b
Manuel <manuel.machado.j.m@gmail.com>
2025-06-13 16:26:16 -0300

    act 130625

--- src/app/pages/informacion/informacion.component.ts
+++ src/app/pages/informacion/informacion.component.ts
@@ -1,15 +1,20 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-informacion',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf, NgTemplateOutlet], // <- Importá NgIf y NgTemplateOutlet para *ngIf con else
   templateUrl: './informacion.component.html',
-  styleUrls: ['./informacion.component.css'], // Corregido el nombre del atributo
+  styleUrls: ['./informacion.component.css'],
 })
 export class InformacionComponent {
   informaciones: { descripcion: string; imagen: string }[] = [
+    {
+      descripcion:
+        'https://play.google.com/store/apps/details?id=com.locucionar.fmloreto1001',
+      imagen: 'assets/images/radio.jpeg',
+    },
     {
       descripcion:
         '✅ ¡Seguimos apostando al deporte!El equipo municipal hizo una gran entrega a la Dirección de Deportes para el campeonato de fútbol:🕒 Relojes digitales🟥🟨 Tarjetas🏳️‍🌈 Banderines⚽ Pelotas🛏️ Camilla🔊 Silbatos¡Gracias a una inversión que potencia el deporte local! ⚽🙌',
@@ -17,24 +22,13 @@ export class InformacionComponent {
     },
     {
       descripcion: '',
-      imagen: 'assets/images/sabado6.jpeg',
-    },
-    {
-      descripcion: '',
       imagen: 'assets/images/sabado2.jpeg',
     },
-    {
-      descripcion:
-        '📣✨ ¡Llega la Gran Feria de Emprendedores Regionales! ✨📣Este 25 de mayo te esperamos desde las 15:30 hs sobre la Av. 25 de Mayo para vivir una tarde llena de talento, creatividad y productos únicos. 🛍️🎨🍪✅ Emprendedores locales✅ Música, color y buena onda✅ Sorteos y sorpresas📍 ¡Sumate con tu familia y amigos y apoyá lo nuestro!',
-      imagen: 'assets/images/sabado7.jpeg',
-    },
-
     {
       descripcion:
         '🚦 ¡Seguimos con la educación vial! 🚸 Estuvimos en la Escuela Ángel Bassi N.º 132, compartiendo una jornada súper educativa con los alumnos. 🧠✨Desde la Dirección de Tránsito de la Municipalidad seguimos recorriendo escuelas, enseñando sobre seguridad vial, respeto y responsabilidad en la vía pública.¡Gracias a toda la comunidad educativa por recibirnos con tanto entusiasmo! 💚🙌',
       imagen: 'assets/images/sabado5.jpeg',
     },
-
     {
       descripcion: '',
       imagen: 'assets/images/info7.jpeg',
@@ -43,19 +37,14 @@ export class InformacionComponent {
       descripcion: '',
       imagen: 'assets/images/info1.jpeg',
     },
-
     {
       descripcion: '',
-
       imagen: 'assets/images/info6.jpeg',
     },
-
     {
       descripcion: '',
-
       imagen: 'assets/images/actu1.jpeg',
     },
-
     {
       descripcion: '',
       imagen: 'assets/images/actu4.jpeg',
@@ -64,7 +53,6 @@ export class InformacionComponent {
       descripcion: '',
       imagen: 'assets/images/info5.jpeg',
     },
-
     {
       descripcion:
         'MORATORIA MUNICIPAL 2025 📝 Ya podés financiar tu deuda municipal, en cuotas  y sin intereses.Tenes tiempo hasta el viernes 31 de Marzo.',
@@ -75,8 +63,6 @@ export class InformacionComponent {
         '🏕️ ¡Información Importante de Hospedajes de Loreto Corrientes para el Verano 2025! 🌞Desde la Municipalidad y la Dirección de Turismo, te compartimos diferentes opciones de estadía para disfrutar al máximo de tus vacaciones en nuestro querido Loreto. 🏖️',
       imagen: 'assets/images/info3.jpg',
     },
-
-    // Agrega más informacion según sea necesario
   ];
 
   openZoom(imagen: string) {
