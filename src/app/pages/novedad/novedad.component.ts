6631d54fb298b5f38afd50da408927259890892a
Manuel <manuel.machado.j.m@gmail.com>
2025-08-16 00:07:56 -0300

    actualizacion 160825

--- src/app/pages/novedad/novedad.component.ts
+++ src/app/pages/novedad/novedad.component.ts
@@ -5,12 +5,12 @@ import { NgClass, NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
 @Component({
   selector: 'app-novedad',
   standalone: true,
-  imports: [NgFor, NgIf, NgTemplateOutlet, NgClass], // <- Importá NgIf y NgTemplateOutlet para *ngIf con else
+  imports: [NgFor, NgIf, NgTemplateOutlet, NgClass, VideoComponent], // <- Importá NgIf y NgTemplateOutlet para *ngIf con else
   templateUrl: './novedad.component.html',
   styleUrls: ['./novedad.component.css'], // corregí styleUrl por styleUrls (plural)
 })
 export class NovedadComponent {
-  novedades = [
+  /*novedades = [
     {
       descripcion:
         '🏥 ¡Loreto sigue avanzando! 🏥💚En el día de hoy, junto a la directora del Hospital J. R. Piñeiro, Dra. Julia Torres de Ortiz, recibimos con gran emoción el nuevo equipamiento médico que formará parte del tan esperado Hospital Moderno J. R. Piñeiro, próximo a inaugurarse.Cada equipo que llega representa una mejora concreta en la salud pública, una inversión en el presente y el futuro de todos queridos Loretanos.Agradecemos profundamente al Gobierno Provincial por trabajar de manera conjunta con nuestra gestión para hacer realidad este gran paso para Loreto.El compromiso compartido entre Provincia y Municipio es el camino para seguir transformando la vida de nuestra comunidad.¡Estamos cada vez más cerca de ver este sueño cumplido!',
@@ -18,7 +18,7 @@ export class NovedadComponent {
     },
 
     // agrega más novedades si querés
-  ];
+  ];*/
 
   activeIndexNovedad = 0;
 
